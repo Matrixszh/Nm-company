26534e10d4389f351b502678ccd4b701355b617b
Zain <116815294+Matrixszh@users.noreply.github.com>
2025-04-14 05:05:47 +0530

    Update page.tsx
    Updated button

--- app/History/page.tsx
+++ app/History/page.tsx
@@ -221,7 +221,7 @@ export default function Home() {
                 className="px-6 py-3 bg-custom-gradient text-white rounded-full shadow-md hover:bg-none hover:text-white border border-1 transition duration-300"
                 onClick={() =>
                   document
-                    .getElementById("history")
+                    .getElementById("Form")
                     ?.scrollIntoView({ behavior: "smooth" })
                 }
               >
