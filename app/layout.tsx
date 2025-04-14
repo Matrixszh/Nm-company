fc90f00660b1f8fc900b153104d2bfe8a1794142
Zain <zainhussaini9898@gmail.com>
2025-04-15 03:09:59 +0530

    meta data and favicon changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,31 +15,31 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://needaloannow.com"),
-  title: "Creative Funding - Flexible Loan Solutions",
+  metadataBase: new URL("https://nm-company.vercel.app/"),
+  title: "NM Company",
   description:
-    "Creative Funding provides trusted loan solutions with an easy application process, reliable service, and 24/7 availability. We offer various types of loans tailored to meet your financial needs.",
+    "NM Company is a Solution for every event",
   openGraph: {
-    title: "Creative Funding | Flexible & Reliable Loan Solutions",
+    title: "NM Company",
     description:
-      "Experience hassle-free lending with Creative Funding. We offer various types of loans with an easy process, trustworthy service, and round-the-clock availability. Your reliable partner in financial solutions.",
+      "NM Company is a Solution for every event",
     images: [
       {
-        url: "/logo2.png",
+        url: "/logo.png",
         width: 1200,
         height: 630,
         alt: "Creative Funding Logo",
       },
     ],
     type: "website",
-    url: "/logo2.png",
+    url: "/logo.png",
   },
   twitter: {
     card: "summary_large_image",
     title: "Creative Funding | Simple & Trusted Loan Solutions",
     description:
       "Looking for flexible loan options? Creative Funding offers various types of loans with an easy process, trustworthy service, and 24/7 support. Your path to reliable financial solutions starts here.",
-    images: ["/logo2.png"],
+    images: ["/logo.png"],
   },
 };
 
